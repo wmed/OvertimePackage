6ea970c45510afdd98cc2bc8518e676ab45dcf53
Daniel Baldonado <thebaldo@ucla.edu>
2024-12-02 12:49:49 -0800

    get height

--- Sources/Overtime/SwiftUI/GetHeightModifier.swift
+++ Sources/Overtime/SwiftUI/GetHeightModifier.swift
@@ -7,10 +7,16 @@
 
 import SwiftUI
 
-public struct GetHeightModifier: ViewModifier {
-    @Binding public var height: CGFloat
+public extension View {
+    func getHeight(_ height: Binding<CGFloat>) -> some View {
+        return self.modifier(GetHeightModifier(height: height))
+    }
+}
 
-    public func body(content: Content) -> some View {
+struct GetHeightModifier: ViewModifier {
+    @Binding var height: CGFloat
+
+    func body(content: Content) -> some View {
         content.background(
             GeometryReader { geo -> Color in
                 DispatchQueue.main.async {
